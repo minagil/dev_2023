6a2483890c8573caa8cf0faa0a96b8eda8dc52d0
mina <rlfalsk0301@gmail.com>
2023-07-08 20:57:34 +0900

    slider 수정

--- Slider/script_02.js
+++ Slider/script_02.js
@@ -2,14 +2,25 @@ const Slider = function (element) {
 	let uicarousel_btn_prev = null;
 	let uicarousel_btn_next = null;
 	let uicarousel_container = null;
-	let uicarousel_item = null;
+	let uicarousel_items = [];
+	let currentIndex = null;
 	let index = 0;
-	let currentIndex = 0;
+
+	const createClones = () => {
+		const nodes = Array.from(uicarousel_items);
+		nodes.forEach((node) => {
+			const clone = node.cloneNode(true);
+			clone.setAttribute("class", "uicarousel_item");
+			uicarousel_container.append(clone);
+		});
+		nodes.reverse().forEach((node) => {
+			const clone = node.cloneNode(true);
+			clone.setAttribute("class", "uicarousel_item");
+			uicarousel_container.prepend(clone);
+		});
+	};
 
 	const getIndex = () => {
-		const uicarousel_items = Array.from(
-			uicarousel_container.querySelectorAll(".uicarousel_item")
-		);
 		uicarousel_items.forEach((uicarousel_item, idx) => {
 			if (uicarousel_item.classList.contains("current")) {
 				index = idx;
@@ -18,66 +29,50 @@ const Slider = function (element) {
 		return index;
 	};
 
+	const setItemIndex = () => {
+		const _index = getIndex();
+		// console.log(_index);
+		moveToslide(_index);
+	};
+
 	const setItems = () => {
 		uicarousel_container.style.transform = `translateX(0)`;
-		const nodes = Array.from(
-			uicarousel_container.querySelectorAll(".uicarousel_item")
-		);
-
-		nodes.forEach((node) => {
-			const clone = node.cloneNode(true);
-			clone.setAttribute("class", "uicarousel_item");
-			uicarousel_container.append(clone);
-		});
-
-		nodes.reverse().forEach((node) => {
-			const clone = node.cloneNode(true);
-			clone.setAttribute("class", "uicarousel_item");
-			uicarousel_container.prepend(clone);
-		});
 	};
 
 	const nextToslide = () => {
-		const uicarousel_items = Array.from(
-			uicarousel_container.querySelectorAll(".uicarousel_item")
-		);
-		const targetIndex = getIndex();
-
 		if (currentIndex >= uicarousel_items.length - 1) {
 			moveToslide(0);
 		} else {
-			moveToslide(targetIndex + 1);
+			moveToslide(currentIndex + 1);
 		}
 	};
 
 	const prevToslide = () => {
-		const uicarousel_items = Array.from(
-			uicarousel_container.querySelectorAll(".uicarousel_item")
-		);
-		const targetIndex = getIndex();
-    
 		if (currentIndex === 0) {
-      moveToslide(uicarousel_items.length - 1);
+			moveToslide(uicarousel_items.length - 1);
 		} else {
-      moveToslide(targetIndex - 1);
+			moveToslide(currentIndex - 1);
 		}
 	};
 
-	const moveToslide = (step) => {
+	const moveToslide = (index) => {
 		const currentItems =
-			uicarousel_container.querySelectorAll(".uicarousel_item")[step];
+			uicarousel_container.querySelectorAll(".uicarousel_item")[index];
+		const ItemWidth =
+			uicarousel_container.querySelectorAll(".uicarousel_item")[0].offsetWidth;
 		const active = uicarousel_container.querySelector(".current");
 
-		if (active) {
-			active.classList.remove("current");
-		}
+		uicarousel_container.style.transform = `translateX(-${
+			ItemWidth * index
+		}px)`;
 
+		if (active) active.classList.remove("current");
 		currentItems.classList.add("current");
 
-		currentIndex = step;
+		currentIndex = index;
 	};
 
-	const onHandlerEvent = () => {
+	const setButton = () => {
 		uicarousel_btn_next.addEventListener("click", () => {
 			nextToslide();
 		});
@@ -90,12 +85,14 @@ const Slider = function (element) {
 		uicarousel_btn_prev = element.querySelector(".uicarousel_btn_prev") || null;
 		uicarousel_btn_next = element.querySelector(".uicarousel_btn_next") || null;
 		uicarousel_container = element.querySelector(".uicarousel_items") || null;
-		uicarousel_item = element.querySelectorAll(".uicarousel_item")[0] || null;
+		uicarousel_items = element.querySelectorAll(".uicarousel_item") || null;
 
+		createClones();
+		setItemIndex();
 		setItems();
 
 		if (uicarousel_btn_next || uicarousel_btn_prev) {
-			onHandlerEvent();
+			setButton();
 		}
 	};
 
--- Slider/script_03.js
+++ Slider/script_03.js
@@ -2,71 +2,105 @@ const Slider = function (element) {
 	let uicarousel_btn_prev = null;
 	let uicarousel_btn_next = null;
 	let uicarousel_container = null;
+	let itemWidth = null;
+	let slideLength = null;
 	let uicarousel_items = [];
-	let currentIndex = null;
+	let currentIndex = 0;
 	let index = 0;
+	let isSliding = false;
+	let isBtnWorking = false;
 
-	const getIndex = () => {
-		uicarousel_items.forEach((uicarousel_item, idx) => {
-			if (uicarousel_item.classList.contains("current")) {
-				index = idx;
-			}
+	const createClones = () => {
+		const nodes = Array.from(uicarousel_items);
+		nodes.forEach((node) => {
+			const clone = node.cloneNode(true);
+			clone.classList.add("clone");
+			uicarousel_container.append(clone);
 		});
-		return index;
+		nodes.reverse().forEach((node) => {
+			const clone = node.cloneNode(true);
+			clone.classList.add("clone");
+			uicarousel_container.prepend(clone);
+		});
 	};
 
-	const setItems = () => {
-		uicarousel_container.style.transform = `translateX(0)`;
-
-		// nodes.forEach((node) => {
-		// 	const clone = node.cloneNode(true);
-		// 	clone.setAttribute("class", "uicarousel_item");
-		// 	uicarousel_container.append(clone);
-		// });
-
-		// nodes.reverse().forEach((node) => {
-		// 	const clone = node.cloneNode(true);
-		// 	clone.setAttribute("class", "uicarousel_item");
-		// 	uicarousel_container.prepend(clone);
-		// });
+	const updateItemWidth = () => {
+		const newItemLength = Array.from(uicarousel_container.children).length;
+		const totalWidthItem = itemWidth * newItemLength;
+		uicarousel_container.style.width = `${totalWidthItem}px`;
 	};
 
-	const nextToslide = () => {
-		if (currentIndex >= uicarousel_items.length - 1) {
-			moveToslide(0);
-		} else {
-			moveToslide(currentIndex + 1);
+	const setInitialItemPos = () => {
+		const cloneItems = Array.from(uicarousel_container.children);
+		const items = cloneItems.filter(
+			(cloneItem) => !cloneItem.classList.contains("clone")
+		);
+		const currentItemWidth = itemWidth * items.length;
+		uicarousel_container.style.transform = `translateX(${-currentItemWidth}px)`;
+		items[0].classList.add("current");
+	};
+
+	const moveToslide = (step) => {
+		if (isSliding) return;
+		isSliding = true;
+		setTimeout(() => {
+			isSliding = false;
+		}, 500);
+
+		uicarousel_container.style.transition = "0.5s";
+		uicarousel_container.style.left = `${-step * itemWidth}px`;
+
+		currentIndex = step;
+
+		uicarousel_container.addEventListener("transitionend", () => {
+			uicarousel_container.style.transition = "0s";
+			uicarousel_container.style.left = `${-currentIndex * itemWidth}`;
+		});
+
+		console.log(currentIndex, step);
+	};
+
+	const prevToSlide = () => {
+		const minIndex = -(slideLength - 1);
+		if (currentIndex <= minIndex) {
+			console.log("ddd");
+			setTimeout(() => {
+				uicarousel_container.style.transition = "0s";
+				uicarousel_container.style.left = "0px";
+				currentIndex = 0;
+			}, 500);
 		}
+		moveToslide(currentIndex - 1);
 	};
 
-	const prevToslide = () => {
-		if (currentIndex === 0) {
-			moveToslide(uicarousel_items.length - 1);
-		} else {
-			moveToslide(currentIndex - 1);
+	const nextToSlide = () => {
+		if (currentIndex >= slideLength - 1) {
+			setTimeout(() => {
+				uicarousel_container.style.transition = "0s";
+				uicarousel_container.style.left = "0px";
+				currentIndex = 0;
+			}, 500);
 		}
+		moveToslide(currentIndex + 1);
 	};
 
-	const moveToslide = (index) => {
-		const currentItems =
-			uicarousel_container.querySelectorAll(".uicarousel_item")[index];
-		const active = uicarousel_container.querySelector(".current");
-
-		if (active) {
-			active.classList.remove("current");
-		}
-
-		currentItems.classList.add("current");
-
-		currentIndex = index;
-	};
-
-	const onHandlerEvent = () => {
+	const setButton = () => {
 		uicarousel_btn_next.addEventListener("click", () => {
-			nextToslide();
+			if (isBtnWorking) return;
+			isBtnWorking = true;
+
+			setTimeout(() => {
+				nextToSlide();
+				isBtnWorking = false;
+			});
 		});
 		uicarousel_btn_prev.addEventListener("click", () => {
-			prevToslide();
+			if (isBtnWorking) return;
+			isBtnWorking = false;
+
+			setTimeout(() => {
+				prevToSlide();
+			});
 		});
 	};
 
@@ -75,12 +109,15 @@ const Slider = function (element) {
 		uicarousel_btn_next = element.querySelector(".uicarousel_btn_next") || null;
 		uicarousel_container = element.querySelector(".uicarousel_items") || null;
 		uicarousel_items = element.querySelectorAll(".uicarousel_item") || null;
+		itemWidth = uicarousel_items[0].offsetWidth;
+		slideLength = uicarousel_items.length;
 
-		getIndex();
-		setItems();
+		createClones();
+		updateItemWidth();
+		setInitialItemPos();
 
 		if (uicarousel_btn_next || uicarousel_btn_prev) {
-			onHandlerEvent();
+			setButton();
 		}
 	};
 
